89665a3e64d317432178411757e6c4624bf24f07
Krunal Vasoya <kvasoya310@hotmail.com>
2023-08-17 22:52:16 -0400

    update

---
+++ out/_next/static/pBMj4fY7U8sR7rpbc6wlh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/985d18ac40573070.css","static/chunks/pages/index-43ce457fbe01e381.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
